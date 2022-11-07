f6bdddc308082219b7c6b8713d667e4efd315460
supertree-emjay <emjay@supertree.co>
2022-11-07 16:02:01 +0900

    feat: setting express server

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+
+// express 서버 객체 생성
+const app = express();
+
+// express 서버 세팅
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+// router setting
+
+const PORT = 4000; // 서버 PORT 지정
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+// 서버 시작
+app.listen(PORT, handleListening());
