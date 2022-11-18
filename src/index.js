501cb03fc4cb978943a105250c0fab7646a07a21
supertree-emjay <emjay@supertree.co>
2022-11-18 14:50:01 +0900

    feat: mongoose setting

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
 import helloRouter from "./routes/hello";
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error);
+db.once("open", () =>
+  console.log(`Connected to mongo server: ${process.env.MONGO_URL}...`)
+);
 
 // express 서버 객체 생성
 const app = express();
@@ -13,14 +28,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(cors());
 
 // router setting
 app.use("/", helloRouter);
 
-const PORT = 4000; // 서버 PORT 지정
-
 const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
+  console.log(`Listening on: http://localhost:${process.env.PORT}`);
 
 // 서버 시작
-app.listen(PORT, handleListening());
+app.listen(process.env.PORT, handleListening());
