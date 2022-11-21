caec3157405f465092ae7fa832b30a0b2df6c5b1
supertree-emjay <emjay@supertree.co>
2022-11-21 16:04:25 +0900

    feat: add join api

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,10 +2,28 @@ import User from "../models/user";
 
 export const joinUser = async (req, res) => {
   try {
+    const { email, password, name, age } = req.body;
+
+    const data = new User({
+      email,
+      password,
+      salt: "123",
+      name,
+      age,
+    });
+
+    // RDB
+    // SELECT => 조회
+    // INSERT => 삽입
+    // UPDATE => 수정
+    // DELETE => 삭제
+
+    await data.save(); // User Schema Insert(삽입)하는 과정
+
     res.send({
       success: true,
       message: null,
-      data: "join user",
+      data,
     });
   } catch (e) {
     res.send({
