76ef8a5e074b06b4a686e0c9e16276601b54b043
supertree-emjay <emjay@supertree.co>
2022-11-29 15:19:12 +0900

    feat: add post board api

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,9 +1,23 @@
+import Board from "../models/board";
+
 export const postBoard = async (req, res) => {
   try {
+    const { title, content } = req.body;
+    const { user } = res.locals;
+
+    const data = new Board({
+      userId: user._id,
+      writer: user.name,
+      title,
+      content,
+    });
+
+    await data.save();
+
     res.send({
       success: true,
       message: null,
-      data: "postBoard",
+      data,
     });
   } catch (e) {
     res.send({
